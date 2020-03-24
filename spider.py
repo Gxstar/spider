2674602fcf369f24036b9c3025de82c06a9c8e5c
Gxstar <gengxing123@qq.com>
2020-03-24 23:58:19 +0800

    尝试vs2019

--- spider.py
+++ spider.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import openpyxl,mysql.connector
+header={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36 Edg/79.0.309.65"}
 # 获得爬取地址
 def getPage(website):
     listnum=[x*25 for x in range(0,10)]
     listurl=[]
     for i in listnum:
         listurl.append("https://movie.douban.com/top250?start="+str(i))
+    print("爬取网址列表获取成功！")
     return listurl
 # 获得网页内容并写入数据库
 def getList(listurl):
@@ -16,9 +18,10 @@ def getList(listurl):
     cursor=conn.cursor()
     cursor.execute('create database if not exists movielist;')
     cursor.execute('use movielist;') 
-    cursor.execute('drop table if exists movieinfo')
-    cursor.execute('create table movieinfo (Name varchar(20) primary key,Image varchar(255),Url varchar(255),Score double,Num_person int,Rating_dou int);')
-    header={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36 Edg/79.0.309.65"}
+    cursor.execute('create table if not exists movieinfo (Name varchar(20) primary key,Image varchar(255),Url varchar(255),Score double,Num_person int,Rating_dou int);')
+    print("数据库创建成功！")
+    global header
+    listnum=0
     for i in listurl:
         page=requests.get(i,headers=header)
         soup=BeautifulSoup(page.text,"lxml")
@@ -29,15 +32,23 @@ def getList(listurl):
             image=str(j.img['src'])
             url=str(j.a['href'])
             score=float(j.select('span.rating_num')[0].string)
-            person=int(j.select('span')[-2:][0].string.split('人')[0])
-            cursor.execute('insert into movieinfo (Name,Image,Url,Score,Num_person,Rating_dou) values (%s,%s,%s,%s,%s,%s)',(name,image,url,score,person,rating))
+            person=int(j.select('div.bd span')[3].string.split('人')[0])
+            listnum+=1
+            cursor.execute('insert ignore into movieinfo (Name,Image,Url,Score,Num_person,Rating_dou) values (%s,%s,%s,%s,%s,%s)',(name,image,url,score,person,rating))
+            print('\r写入第{0}条成功！'.format(listnum),end='')
     conn.commit()
     cursor.close()
     conn.close()
+def searchMovie(movieName):
+    i=input("请输入操作指令：")
+    global header
+    if i=='1':
+        url="https://www.dy2018.com"
+        page=requests.get(url,headers=header)
+        soup=BeautifulSoup(page.text,"lxml")
 # 主函数
 def main():
-    wname="douban"
-    listurl=getPage(wname)
-    getList(listurl)
+    url=getPage('hh')
+    getList(url)
 if __name__=="__main__":
     main()
